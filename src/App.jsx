c4454b4c7b6df9101c8ac4c9e5c6a1e75d82a0e5
Hamzhgg <gharibehhamzh@gmail.com>
2024-11-03 13:46:12 +0300

    first-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-// src/App.jsx
 export default function App(){
+const todo  = {
+  id : 1,
+  text: "Learn React JSX",
+  done: true
+}
+
+
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <>
+      <h1 className="hello">Hello world!</h1>
       <h2>Hello universe!</h2>
+      <p>{todo.text}</p>
       <hr/>
       <br/>
-      <input type="text" />
-    </div>
-  );
+      <label htmlFor="name"></label>
+      <input tybe="text" name="name" id="name" />
+    </>
+  )
 };
