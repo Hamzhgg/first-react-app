7f377221d14919785a5e32afb4626d6da118ea27
Hamzhgg <gharibehhamzh@gmail.com>
2024-11-03 13:18:49 +0300

    fixed issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // src/App.jsx
-
 export default function App(){
-
   return (
-    <h1>Hello, world!</h1>
+    <div>
+      <h1>Hello world!</h1>
+      <h2>Hello universe!</h2>
+      <hr/>
+      <br/>
+      <input type="text" />
+    </div>
   );
 };
